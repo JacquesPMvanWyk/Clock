fe9c3c2976b472b103a72e49f4fe27f12657f002
Jacques van wyk <71254161+JacquesPMvanWyk@users.noreply.github.com>
2021-09-22 20:15:36 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+type="text/javascript"
+function clock() {
+    let hours = document.getElementById('hour')
+    let min = document.getElementById('min')
+    let seconds = document.getElementById('seconds')
+    let ampm = document.getElementById('ampm')
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    var am ="AM";
+
+    if(h >12){
+        h = h -12
+        var am = "PM"
+    }
+
+    hours.innerHTML = h;
+    min.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am;
+
+
+
+}
+
+var interval = setInterval(clock, 1000);
+    
